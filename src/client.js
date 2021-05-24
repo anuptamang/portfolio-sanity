b85eedeb2fa45bb91dc34fd871c38fe957a5aeeb
Anup Kumar Tamang <anupkumartamang@anuptamang-mbp-3.local>
2021-05-24 17:26:33 +0545

    readme added

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: 'o5lg176f',
+  projectId: 'o5lg176f', // YOUR_SANITY_PROJECT_ID
   dataset: 'production',
 })
